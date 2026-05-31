d4b74c69fc4636cbce3d22de8b0604f7f2b35c09
Luizband <band.luizfelipe@gmail.com>
2026-05-31 18:42:16 -0300

    Add files via upload

---
+++ badges.js
@@ -0,0 +1,47 @@
+export const INFO_BADGES = {
+    "badge-001": {
+        nome: "Starter Pack",
+        descricao: "Completou as 52 primeiras cartas da coleção.",
+        imagem: "badge-001.png"
+    },
+    "badge-002": {
+        nome: "Coleção SP",
+        descricao: "Completou as cartas 020, 036 e 045.",
+        imagem: "badge-002.png"
+    },
+    "badge-003": {
+        nome: "Viciado",
+        descricao: "Fez login durante 20 dias seguidos.",
+        imagem: "badge-003.png"
+    },
+    "badge-004": {
+        nome: "Troca Troca",
+        descricao: "Fez 20 trocas ao todo.",
+        imagem: "badge-004.png"
+    },
+    "badge-005": {
+        nome: "Guerreiro",
+        descricao: "Ganhou 30 batalhas.",
+        imagem: "badge-005.png"
+    },
+    "badge-006": {
+        nome: "Reciclador",
+        descricao: "Reciclou 30 cartas.",
+        imagem: "badge-006.png"
+    },
+    "badge-007": {
+        nome: "Consumidor",
+        descricao: "Gastou 3000 moedas na loja.",
+        imagem: "badge-007.png"
+    },
+    "badge-008": {
+        nome: "Pacotinhos!",
+        descricao: "Abriu 30 pacotes.",
+        imagem: "badge-008.png"
+    },
+    "badge-009": {
+        nome: "Coleção Natal",
+        descricao: "Completou as cartas de natal 009 e 027.",
+        imagem: "badge-009.png"
+    }
+};

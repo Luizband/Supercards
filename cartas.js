f3998f692644dcb07fe2bbef2c857c9047337971
Luizband <band.luizfelipe@gmail.com>
2026-05-31 13:09:48 -0300

    Add files via upload

---
+++ cartas.js
@@ -0,0 +1,117 @@
+// Banco de Dados Oficial das Cartas do Supercards
+// Aqui você vai preencher os dados reais de todas as 52 cartas!
+
+export const INFO_CARTAS = {
+    "001": { nome: "Cuba Livre", raridade: "Comum", pontos: 1, aura: 57, fofura: 31, diversao: 12, aleatoriedade: 20, imagem: "001.png" },
+    
+    "002": { nome: "João do Circo", raridade: "Comum", pontos: 1, aura: 45, fofura: 25, diversao: 22, aleatoriedade: 28, imagem: "002.png" },
+    
+    "003": { nome: "Torta da Dona Rose", raridade: "Comum", pontos: 1, aura: 44, fofura: 29, diversao: 31, aleatoriedade: 8, imagem: "003.png" },
+
+    "004": { nome: "Pratinho de Churrasco", raridade: "Comum", pontos: 1, aura: 19, fofura: 16, diversao: 55, aleatoriedade: 21, imagem: "004.png" },
+
+    "005": { nome: "Cabeçudinhos", raridade: "Comum", pontos: 1, aura: 10, fofura: 50, diversao: 40, aleatoriedade: 9, imagem: "005.png" },
+
+    "006": { nome: "Luiz Fichado", raridade: "Comum", pontos: 1, aura: 48, fofura: 5, diversao: 5, aleatoriedade: 46, imagem: "006.png" },
+
+    "007": { nome: "Tudo nos Conformes", raridade: "Comum", pontos: 1, aura: 64, fofura: 18, diversao: 4, aleatoriedade: 10, imagem: "007.png" },
+
+    "008": { nome: "Gatos Fofoqueiros", raridade: "Comum", pontos: 2, aura: 39, fofura: 36, diversao: 26, aleatoriedade: 65, imagem: "008.png" },
+
+    "009": { nome: "Luiz HO HO HO", raridade: "Comum", pontos: 2, aura: 11, fofura: 42, diversao: 6, aleatoriedade: 7, imagem: "009.png" },
+
+    "010": { nome: "Mijada Coletiva", raridade: "Comum", pontos: 2, aura: 9, fofura: 19, diversao: 54, aleatoriedade: 40, imagem: "010.png" },
+
+    "011": { nome: "Pra Não Perder o Costume", raridade: "Comum", pontos: 2, aura: 26, fofura: 14, diversao: 39, aleatoriedade: 22, imagem: "011.png" },
+
+    "012": { nome: "De Novo, a Amizade", raridade: "Comum", pontos: 2, aura: 24, fofura: 25, diversao: 32, aleatoriedade: 35, imagem: "012.png" },
+
+    "013": { nome: "Só na Amizade", raridade: "Comum", pontos: 2, aura: 59, fofura: 46, diversao: 11, aleatoriedade: 12, imagem: "013.png" },
+
+    "014": { nome: "Gugu S2", raridade: "Comum", pontos: 2, aura: 68, fofura: 50, diversao: 23, aleatoriedade: 15, imagem: "014.png" },
+
+    "015": { nome: "Abaporu Maconheiro", raridade: "Comum", pontos: 2, aura: 21, fofura: 19, diversao: 61, aleatoriedade: 47, imagem: "015.png" },
+
+    "016": { nome: "Melejorge", raridade: "Comum", pontos: 2, aura: 2, fofura: 23, diversao: 59, aleatoriedade: 37, imagem: "016.png" },
+
+    "017": { nome: "Gato Laranja Aleatório", raridade: "Comum", pontos: 2, aura: 45, fofura: 55, diversao: 7, aleatoriedade: 8, imagem: "017.png" },
+
+    "018": { nome: "Almoço de Uma Terça", raridade: "Comum", pontos: 2, aura: 37, fofura: 13, diversao: 44, aleatoriedade: 49, imagem: "018.png" },
+
+    "019": { nome: "Nhoque de Pica", raridade: "Comum", pontos: 2, aura: 10, fofura: 43, diversao: 14, aleatoriedade: 68, imagem: "019.png" },
+
+    "020": { nome: "João Pensativo em SP", raridade: "Comum", pontos: 2, aura: 12, fofura: 16, diversao: 33, aleatoriedade: 42, imagem: "020.png" },
+
+    "021": { nome: "Salve o Tricolor Paulista", raridade: "Comum", pontos: 2, aura: 44, fofura: 33, diversao: 21, aleatoriedade: 27, imagem: "021.png" },
+
+    "022": { nome: "Jedi Fred", raridade: "Comum", pontos: 2, aura: 36, fofura: 59, diversao: 52, aleatoriedade: 11, imagem: "022.png" },
+
+    "023": { nome: "Deu Ruim", raridade: "Comum", pontos: 1, aura: 55, fofura: 9, diversao: 7, aleatoriedade: 17, imagem: "023.png" },
+
+    "024": { nome: "Faz o L", raridade: "Comum", pontos: 1, aura: 13, fofura: 13, diversao: 13, aleatoriedade: 13, imagem: "024.png" },
+
+    "025": { nome: "Gato Pinguço", raridade: "Comum", pontos: 1, aura: 24, fofura: 33, diversao: 29, aleatoriedade: 65, imagem: "025.png" },
+
+    "026": { nome: "O Que Trazes Pra Mim?", raridade: "Comum", pontos: 2, aura: 10, fofura: 60, diversao: 40, aleatoriedade: 8, imagem: "026.png" },
+
+    "027": { nome: "Jessie HO HO HO", raridade: "Comum", pontos: 2, aura: 15, fofura: 67, diversao: 40, aleatoriedade: 11, imagem: "027.png" },
+
+    "028": { nome: "Jessie Fugitiva N°2", raridade: "Comum", pontos: 2, aura: 60, fofura: 19, diversao: 31, aleatoriedade: 12, imagem: "028.png" },
+
+    "029": { nome: "Jessie Fugitiva N°1", raridade: "Comum", pontos: 2, aura: 51, fofura: 23, diversao: 22, aleatoriedade: 48, imagem: "029.png" },
+
+    "030": { nome: "Jessie da Zona Leste", raridade: "Comum", pontos: 1, aura: 54, fofura: 57, diversao: 32, aleatoriedade: 20, imagem: "030.png" },
+
+    "031": { nome: "Ser Humane", raridade: "Rara", pontos: 3, aura: 23, fofura: 28, diversao: 36, aleatoriedade: 79, imagem: "031.png" },
+
+    "032": { nome: "Isso Aqui é Rock", raridade: "Rara", pontos: 3, aura: 76, fofura: 20, diversao: 66, aleatoriedade: 47, imagem: "032.png" },
+
+    "033": { nome: "Gripe Masculina", raridade: "Rara", pontos: 3, aura: 14, fofura: 27, diversao: 19, aleatoriedade: 71, imagem: "033.png" },
+
+    "034": { nome: "Nada a Comentar", raridade: "Rara", pontos: 3, aura: 17, fofura: 24, diversao: 24, aleatoriedade: 72, imagem: "034.png" },
+
+    "035": { nome: "Truco!", raridade: "Rara", pontos: 3, aura: 59, fofura: 53, diversao: 35, aleatoriedade: 42, imagem: "035.png" },
+
+    "036": { nome: "João no Lixo em SP", raridade: "Rara", pontos: 3, aura: 18, fofura: 32, diversao: 51, aleatoriedade: 74, imagem: "036.png" },
+
+    "037": { nome: "Sai Zoião Jr", raridade: "Rara", pontos: 3, aura: 79, fofura: 52, diversao: 45, aleatoriedade: 21, imagem: "037.png" },
+
+    "038": { nome: "Hora da Natação", raridade: "Rara", pontos: 3, aura: 27, fofura: 59, diversao: 40, aleatoriedade: 64, imagem: "038.png" },
+
+    "039": { nome: "Augusto Tigrinho", raridade: "Rara", pontos: 3, aura: 56, fofura: 53, diversao: 61, aleatoriedade: 38, imagem: "039.png" },
+
+    "040": { nome: "Au Au", raridade: "Rara", pontos: 4, aura: 40, fofura: 69, diversao: 35, aleatoriedade: 58, imagem: "040.png" },
+
+    "041": { nome: "Split - Mom Jeans", raridade: "Rara", pontos: 4, aura: 68, fofura: 20, diversao: 41, aleatoriedade: 37, imagem: "041.png" },
+
+    "042": { nome: "Sheik Helias", raridade: "Rara", pontos: 3, aura: 39, fofura: 70, diversao: 44, aleatoriedade: 47, imagem: "042.png" },
+
+    "043": { nome: "Bate Forte o Tambor", raridade: "Rara", pontos: 3, aura: 62, fofura: 50, diversao: 43, aleatoriedade: 67, imagem: "043.png" },
+
+    "044": { nome: "2006 Feelings", raridade: "Rara", pontos: 3, aura: 2, fofura: 63, diversao: 62, aleatoriedade: 45, imagem: "044.png" },
+
+    "045": { nome: "João de 4 em SP", raridade: "Rara", pontos: 3, aura: 30, fofura: 26, diversao: 75, aleatoriedade: 42, imagem: "045.png" },
+
+    "046": { nome: "Mikaoinese", raridade: "Lendária", pontos: 5, aura: 55, fofura: 68, diversao: 86, aleatoriedade: 31, imagem: "046.png" },
+
+    "047": { nome: "Garoto de Ipanema", raridade: "Lendária", pontos: 5, aura: 66, fofura: 81, diversao: 40, aleatoriedade: 19, imagem: "047.png" },
+
+    "048": { nome: "Cogumelo Trip", raridade: "Lendária", pontos: 5, aura: 77, fofura: 40, diversao: 93, aleatoriedade: 60, imagem: "048.png" },
+
+    "049": { nome: "Break Dance", raridade: "Lendária", pontos: 5, aura: 91, fofura: 38, diversao: 62, aleatoriedade: 76, imagem: "049.png" },
+
+    "050": { nome: "Jorge Trump", raridade: "Lendária", pontos: 5, aura: 45, fofura: 75, diversao: 32, aleatoriedade: 87, imagem: "050.png" },
+
+    "051": { nome: "Copão de Campari", raridade: "Épica", pontos: 7, aura: 62, fofura: 61, diversao: 80, aleatoriedade: 90, imagem: "051.png" },
+
+    "052": { nome: "Jessie Malfoy", raridade: "Épica", pontos: 7, aura: 90, fofura: 44, diversao: 65, aleatoriedade: 88, imagem: "052.png" }
+}; // Não esqueça que o arquivo termina com essa chave e ponto e vírgula!
+  
+
+// Tabela de fragmentos globais (já deixamos salvo aqui também)
+export const TABELA_FRAGMENTOS = {
+    "Comum": 2,
+    "Rara": 10,
+    "Épica": 100,
+    "Lendária": 40
+};

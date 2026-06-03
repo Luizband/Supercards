10e55cb54f0a99e3a3d56cc1a9ca220c44ef33ed
Luizband <band.luizfelipe@gmail.com>
2026-06-03 11:44:19 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "supercards-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./fundotela.png"
+  // Depois podemos adicionar mais coisas aqui se quiser
+];
+
+// Instala o Service Worker e salva os arquivos iniciais
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Intercepta as requisições para deixar o jogo mais rápido
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Retorna do cache se existir, senão baixa da internet
+      return response || fetch(event.request);
+    })
+  );
+});
